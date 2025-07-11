125ff1508690875f79ef0903b79a174cadd8b849
jakesanghavi <jake.sanghavi@gmail.com>
2025-07-11 14:08:39 -0400

    Simplify HomePage for further templating ease

---
+++ src/components/LeftBar.jsx
@@ -0,0 +1,89 @@
+import PlacesTreeView from "../components/PlacesTreeView";
+import Login from "../components/Login"
+import L from "leaflet";
+import "../index.css";
+
+// Fix Leaflet's marker icon paths
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+const openLoginModal = (email) => {
+    document.getElementById('sign-in-modal').style.display = 'block';
+    document.getElementById('signUpEmail').value = email;
+};
+
+function LeftBar({setIsModalOpen, setIsFlightModalOpen, setSelectedPlaceForView, setIsViewModalOpen, places, handleLoginSuccess, getUserID, loggedInUser}) {
+
+    const onSelectPlaceFromTree = (placeData) => {
+        setSelectedPlaceForView(placeData);
+        setIsViewModalOpen(true);
+    };
+
+    return (
+        <div style={{ width: "15%", backgroundColor: "#f8f9fa", padding: "10px", overflowY: "auto", color: "black" }}>
+            <Login onLoginSuccess={handleLoginSuccess} uid={getUserID} openLoginModal={openLoginModal} />
+            <div id="signIn">
+                {!loggedInUser?.email ? (
+                    <button onClick={openLoginModal}>Sign In to Start Planning!</button>
+                ) : (
+                    <span>{loggedInUser.username}</span>
+                )}
+            </div>
+            {loggedInUser?.email && (
+                <button
+                    onClick={() => {
+                        setIsModalOpen(true);
+                        setForm({
+                            continent: "",
+                            country: "",
+                            city: "",
+                            name: "",
+                            lat: "",
+                            lng: "",
+                            notes: "",
+                        });
+                        setFile(null);
+                    }}
+                    style={{
+                        width: "100%",
+                        padding: "10px",
+                        marginBottom: "15px",
+                        background: "#007bff",
+                        color: "white",
+                        border: "none",
+                        borderRadius: "5px",
+                        cursor: "pointer",
+                        boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
+                    }}
+                >
+                    Add Place
+                </button>
+            )}
+            <button
+                onClick={() => {
+                    setIsFlightModalOpen(true);
+                }}
+                style={{
+                    width: "100%",
+                    padding: "10px",
+                    marginBottom: "15px",
+                    background: "#007bff",
+                    color: "white",
+                    border: "none",
+                    borderRadius: "5px",
+                    cursor: "pointer",
+                    boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
+                }}
+            >
+                Plan Flight
+            </button>
+            <PlacesTreeView data={places} onSelectPlace={onSelectPlaceFromTree} />
+        </div>
+    );
+}
+
+export default LeftBar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState, useCallback } from "react";
-import PlacesTreeView from "../components/PlacesTreeView";
-import Login from "../components/Login"
 import L from "leaflet";
 import { getPlaces } from "../api";
 import AddPlaceMapView from "../components/AddPlaceMapView";
 import "../index.css";
+import LeftBar from "../components/LeftBar";
 
 // Fix Leaflet's marker icon paths
 delete L.Icon.Default.prototype._getIconUrl;
@@ -14,21 +13,11 @@ L.Icon.Default.mergeOptions({
     shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
 });
 
-const openLoginModal = (email) => {
-    document.getElementById('sign-in-modal').style.display = 'block';
-    document.getElementById('signUpEmail').value = email;
-};
-
 function HomePage() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isFlightModalOpen, setIsFlightModalOpen] = useState(false);
     const [selectedPlaceForView, setSelectedPlaceForView] = useState(null);
     const [isViewModalOpen, setIsViewModalOpen] = useState(false);
-
-    const onSelectPlaceFromTree = (placeData) => {
-        setSelectedPlaceForView(placeData);
-        setIsViewModalOpen(true);
-    };
 
     const generateUserID = () => {
         return 'user_' + Math.random().toString(36).substring(2, 15);
@@ -159,71 +148,13 @@ function HomePage() {
             }
         };
 
-        fetchData(); // Call the asynchronous function
+        fetchData();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
         <div style={{ display: "flex", height: "100vh", width: "100vw" }}>
-            <div style={{ width: "15%", backgroundColor: "#f8f9fa", padding: "10px", overflowY: "auto", color: "black" }}>
-                <Login onLoginSuccess={handleLoginSuccess} uid={getUserID} openLoginModal={openLoginModal} />
-                <div id="signIn">
-                    {!loggedInUser?.email ? (
-                        <button onClick={openLoginModal}>Sign In to Start Planning!</button>
-                    ) : (
-                        <span>{loggedInUser.username}</span>
-                    )}
-                </div>
-                {loggedInUser?.email && (
-                    <button
-                        onClick={() => {
-                            setIsModalOpen(true);
-                            setForm({
-                                continent: "",
-                                country: "",
-                                city: "",
-                                name: "",
-                                lat: "",
-                                lng: "",
-                                notes: "",
-                            });
-                            setFile(null);
-                        }}
-                        style={{
-                            width: "100%",
-                            padding: "10px",
-                            marginBottom: "15px",
-                            background: "#007bff",
-                            color: "white",
-                            border: "none",
-                            borderRadius: "5px",
-                            cursor: "pointer",
-                            boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
-                        }}
-                    >
-                        Add Place
-                    </button>
-                )}
-                <button
-                    onClick={() => {
-                        setIsFlightModalOpen(true);
-                    }}
-                    style={{
-                        width: "100%",
-                        padding: "10px",
-                        marginBottom: "15px",
-                        background: "#007bff",
-                        color: "white",
-                        border: "none",
-                        borderRadius: "5px",
-                        cursor: "pointer",
-                        boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
-                    }}
-                >
-                    Plan Flight
-                </button>
-                <PlacesTreeView data={places} onSelectPlace={onSelectPlaceFromTree} />
-            </div>
+            <LeftBar setIsModalOpen={setIsModalOpen} setIsFlightModalOpen={setIsFlightModalOpen} setSelectedPlaceForView={setSelectedPlaceForView} setIsViewModalOpen={setIsViewModalOpen} places={places} handleLoginSuccess={handleLoginSuccess} getUserID={getUserID} loggedInUser={loggedInUser}></LeftBar>
             <AddPlaceMapView loggedInUser={loggedInUser} places={places} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} isFlightModalOpen={isFlightModalOpen} setIsFlightModalOpen={setIsFlightModalOpen} selectedPlaceForView={selectedPlaceForView} setSelectedPlaceForView={setSelectedPlaceForView} isViewModalOpen={isViewModalOpen} setIsViewModalOpen={setIsViewModalOpen}></AddPlaceMapView>
         </div>
     );
