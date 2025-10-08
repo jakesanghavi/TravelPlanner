10eca3dc8153e0002f6c754be8df7a1ef2286eb7
jakesanghavi <jake.sanghavi@gmail.com>
2025-10-08 13:51:02 -0400

    Styling changes

--- src/components/AddPlaceMapView.jsx
+++ src/components/AddPlaceMapView.jsx
@@ -43,7 +43,7 @@ function AddPlaceMapView({ places, isModalOpen, setIsModalOpen, isFlightModalOpe
     const bounds = [southWest, northEast];
 
     return (
-        <div style={{ height: "100%", width: "100%", position: "fixed"}}>
+        <div style={{ height: "100%", width: "100%", position: "fixed" }}>
             {/* Right Panel - 85% */}
             <div style={{ height: "100%", width: "100%" }}>
                 <MapContainer
@@ -69,9 +69,16 @@ function AddPlaceMapView({ places, isModalOpen, setIsModalOpen, isFlightModalOpe
             maxZoom={20}
           /> */}
                     {/* Forest gradient style */}
-                    <TileLayer
+                    {/* <TileLayer
                         url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}"
                         attribution="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community"
+                    /> */}
+                    {/* More Chill Light Mode - Can make light with css*/}
+                    <TileLayer
+                        url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                        subdomains="abcd"
+                        maxZoom={20}
                     />
                     {markers.map((m, idx) => (
                         <Marker key={idx} position={m.position} icon={emojiIcon}>
@@ -130,7 +137,7 @@ function AddPlaceMapView({ places, isModalOpen, setIsModalOpen, isFlightModalOpe
             <Modal isOpen={isFlightModalOpen} onClose={() => setIsFlightModalOpen(false)}>
                 <FlightSearchForm></FlightSearchForm>
             </Modal>
-            <FilterPinsModal isOpen={isFiltersModalOpen} 
+            <FilterPinsModal isOpen={isFiltersModalOpen}
                 onClose={() => setIsFiltersModalOpen(false)}
                 filters={filters}
                 setFilters={setFilters}>
